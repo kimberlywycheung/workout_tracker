af716e973a6a55d788c9658a5839483b7580d6ae
kimberlywycheung <kimberlywycheung@gmail.com>
2023-01-10 10:57:18 -0800

    db files

---
+++ components/Graph.jsx
@@ -0,0 +1,7 @@
+import { Container } from '@mui/material';
+
+export default function List() {
+  return (
+    <Container>list</Container>
+  );
+}
---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS workouts;
+
+CREATE DATABASE workouts;
+\connect workouts;
+
+CREATE TABLE urls (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(1000) NOT NULL,
+  title VARCHAR(1000) NOT NULL,
+  category VARCHAR(1000) NOT NULL,
+  length INTEGER NOT NULL,
+  instructor VARCHAR(1000) NOT NULL
+)
+
+CREATE TABLE workouts (
+  id SERIAL PRIMARY KEY,
+  url_id INTEGER NOT NULL,
+  difficulty INTEGER DEFAULT 0,
+  rating INTEGER DEFAULT 0,
+  notes VARCHAR DEFAULT '',
+  FOREIGN KEY (url_id) REFERENCES urls(id)
+)
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(1000) NOT NULL,
+  workout_id INTEGER NOT NULL,
+  FOREIGN KEY (workout_id) REFERENCES workouts(id)
+)
